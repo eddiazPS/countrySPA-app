6cadb49bf5928967cb193a0516ad390c40c67eca
Edgar Hernando Diaz <edgarh2dm3@gmail.com>
2024-07-04 18:59:26 -0500

    localStorage

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -20,6 +20,18 @@ export class CountriesService {
   }
 
   constructor(private http: HttpClient) {
+    this.loadFromLocalStorage();
+  }
+
+
+  private saveToLocalStorage(){
+    localStorage.setItem('cacheStore',JSON.stringify(this.cacheStore));
+  }
+
+  private loadFromLocalStorage(){
+    if (!localStorage.getItem('cacheStore')) return;
+
+    this.cacheStore = JSON.parse (localStorage.getItem('cacheStore')!);
   }
 
   private getCountriesRequest (url: string): Observable<Country[]> {
@@ -46,7 +58,8 @@ return this.http.get<Country[]> (url)
     const url = `${this.apiUrl}/capital/${term}`;
     return this.getCountriesRequest(url)
     .pipe(
-      tap(countries => this.cacheStore.byCapital = {term , countries})
+      tap(countries => this.cacheStore.byCapital = {term , countries}),
+      tap( () => this.saveToLocalStorage() )
     );
   }
 
@@ -56,7 +69,8 @@ serachCountry (term: string):Observable<Country[]>{
   const url = `${this.apiUrl}/name/${term}`;
   return  this.getCountriesRequest(url)
   .pipe(
-    tap(countries => this.cacheStore.byCountries = {term , countries})
+    tap(countries => this.cacheStore.byCountries = {term , countries}),
+    tap( () => this.saveToLocalStorage() )
   );
 
 }
@@ -69,7 +83,8 @@ serachRegion (region: Region):Observable<Country[]>{
   const url = `${this.apiUrl}/region/${region}`;
     return this.getCountriesRequest(url)
     .pipe(
-      tap(countries => this.cacheStore.byRegion = {region , countries})
+      tap(countries => this.cacheStore.byRegion = {region , countries}),
+      tap( () => this.saveToLocalStorage() ),
     );
 
 }
